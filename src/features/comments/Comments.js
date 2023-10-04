8a8a490f7e747d5b121423e61e82147b9e61c4f1
David D <dailleredavid@gmail.com>
2023-10-04 19:14:29 +0530

    add reply section in comments

--- src/features/comments/Comments.js
+++ src/features/comments/Comments.js
@@ -1,7 +1,15 @@
+import { useEffect, useState } from 'react';
 import commentsCSS from './comments.module.css';
 import ReactMarkdown from "react-markdown";
 
 function Comments({ commentList }) {
+	const [showReplies, setShowReplies] = useState(false);
+
+	const handleReplies = () => {
+		setShowReplies(!showReplies);
+	}
+
+
 	if (commentList.length === 0) {
 		return
 	}
@@ -10,12 +18,27 @@ function Comments({ commentList }) {
 		<div className={commentsCSS.container}>
 			<h3>Comments: </h3>
 			<div className={commentsCSS.comments}>
-				{commentList.map(comment => {
+				{commentList.map((comment, index) => {
 					return (
 						<div className={commentsCSS.comment}>
 							<p className={commentsCSS.author}>{comment.author}</p>
 							{/* Comments may be written in Markdown, This component will transform MD into html:*/}
 							<ReactMarkdown className={commentsCSS.text}>{comment.text}</ReactMarkdown>
+							<p className={commentsCSS.replyButton} onClick={handleReplies}>{showReplies?'Hide Reply':'Show Reply'}</p>
+							<div className={showReplies? commentsCSS.reply: commentsCSS.hideReply}>
+								{comment.replies?.map(reply => {
+									return (
+										<div className={commentsCSS.comment}>
+											<p className={commentsCSS.author}>{reply.data.author}</p>
+											{/* Comments may be written in Markdown, This component will transform MD into html:*/}
+											<ReactMarkdown className={commentsCSS.text}>{reply.data.body}</ReactMarkdown>
+										</div>
+									)
+
+								})}
+							</div>
+
+
 						</div>
 					)
 				})}
